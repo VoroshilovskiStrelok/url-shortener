bc711569f892418e6a538ff7a744ee22db4b64bc
Nik <nyakhno@inbox.ru>
2026-04-12 21:47:17 +0700

    Initial commit

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+)
+
+func main() {
+cfg =: config.MustLoad()
+
+fmt.Println(cfg)
+
+// TODO: init logger: slog
+
+
+// TODO: init storage:sqlite
+
+
+// TODO: init router:chi, "chi render"
+
+
+// TODO: run server
+
+}
