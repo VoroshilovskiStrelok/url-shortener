b1df8e47e0d2f05b985fd7a3a05affc691dc55a0
Nik <nyakhno@inbox.ru>
2026-04-13 02:46:41 +0700

    Add sqlite storage methods: SaveURL and GetURL

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,14 +1,22 @@
 package config
 
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
 type Config struct {
-	Env string `yaml:"env" env-default:"local"`
+	Env         string `yaml:"env" env-default:"local"`
 	StoragePath string `yaml:"storage_path" env-required:"true"`
-	HTTPServer `yaml:"http_server"`
+	HTTPServer  `yaml:"http_server"`
 }
 
 type HTTPServer struct {
-	Address string `yaml:"address" env-default:"localhost:8080"`
-	Timeout time.Duration `yaml:"timeout" env-default:"10s"`
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"10s"`
 	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
 }
 
@@ -19,7 +27,7 @@ func MustLoad() *Config {
 	}
 
 	// check if file exists
-	if _, err := os.Stat(ConfigPath); os.IsNotExist(err) {
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
 		log.Fatalf("config file does not exist: %s", configPath)
 	}
 
